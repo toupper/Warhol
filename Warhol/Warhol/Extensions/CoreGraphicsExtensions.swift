45dc33051f32f77c8d4eb7c3f35ef604d8e4c983
Cesar Vargas Casaseca <cesar.vargas-casaseca@weltn24.de>
2020-04-04 20:21:19 +0200

    add extension file

---
+++ Warhol/Warhol/Extensions/CoreGraphicsExtensions.swift
@@ -0,0 +1,29 @@
+import CoreGraphics
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func * (left: CGPoint, right: CGFloat) -> CGPoint {
+  return CGPoint(x: left.x * right, y: left.y * right)
+}
+
+extension CGSize {
+  var cgPoint: CGPoint {
+    return CGPoint(x: width, y: height)
+  }
+}
+
+extension CGPoint {
+  var cgSize: CGSize {
+    return CGSize(width: x, height: y)
+  }
+
+  func absolutePoint(in rect: CGRect) -> CGPoint {
+    return CGPoint(x: x * rect.size.width, y: y * rect.size.height) + rect.origin
+  }
+}
